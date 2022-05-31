6abf83f81d4397fdc4ede213dbb50974a00450a6
Suiiyu <867077816@qq.com>
2022-05-31 10:00:24 +0800

    upload train test

---
+++ code/test_LA_semi_contrast.py
@@ -0,0 +1,244 @@
+﻿import argparse
+import torch
+from networks.vnet_center import VNet
+import h5py,cv2
+import math
+import nibabel as nib
+import numpy as np
+from medpy import metric
+from utils.measures import dc,jc, hd95,asd,hd
+from surface_distance import metrics as surf_metric
+import torch.nn.functional as F
+from tqdm import tqdm
+import os
+import pandas as pd
+from collections import OrderedDict
+from skimage.measure import label
+
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+"""
+Testing
+Ref:
+Shape-Aware Organ Segmentation by Predicting Signed Distance Maps
+https://arxiv.org/abs/1912.03849
+"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--root_path', type=str, default='../data/2018LA_Seg_Training Set/', help='data path')
+parser.add_argument('--model', type=str,  default='2022-05-28-15-22-04', help='model_name')
+parser.add_argument('--gpu', type=str,  default='2', help='GPU to use')
+parser.add_argument('--normalization', type=str,  default='batchnorm', help='normalization to use')
+parser.add_argument('--epoch_num', type=int,  default='6000', help='checkpoint to use')
+parser.add_argument('--spacing', type=float,  default=1, help='voxel space')
+parser.add_argument('--post', type=int, default=True, help='whether use cca')
+
+FLAGS = parser.parse_args()
+
+os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.gpu
+
+
+snapshot_path = "../semi_model/SCC/"+FLAGS.model+"/"
+test_save_path = "../semi_model/prediction/"+FLAGS.model+"/"
+
+if not os.path.exists(test_save_path):
+    os.makedirs(test_save_path)
+
+num_classes = 2
+
+patch_shape=(112, 112,80)
+
+with open( '../data/test.list', 'r') as f:
+    image_list = f.readlines()# [:4]
+    image_list = [FLAGS.root_path + item.replace('\n', '') + "/mri_norm2.h5" for item in image_list]
+
+def test_calculate_metric(epoch_num):
+    net = VNet(n_channels=1, n_classes=num_classes, normalization=FLAGS.normalization,has_dropout=False,has_triup=False).cuda()
+    save_mode_path = os.path.join(snapshot_path, 'iter_' + str(epoch_num) + '.pth')
+    net.load_state_dict(torch.load(save_mode_path))
+    print("init weight from {}".format(save_mode_path))
+    net.eval()
+
+
+    avg_metric = dist_test_all_case(net, image_list, num_classes=num_classes,
+                                        save_result=False, test_save_path=test_save_path,
+                                        has_post=FLAGS.post)
+
+
+    return avg_metric
+
+
+def dist_test_all_case(net, image_list, num_classes, save_result=True, test_save_path=None, has_post=False):
+    total_metric = 0.0
+    metric_dict = OrderedDict()
+    metric_dict['name'] = list()
+    metric_dict['dice'] = list()
+    metric_dict['jaccard'] = list()
+    metric_dict['asd'] = list()
+    metric_dict['95hd'] = list()
+    metric_dict['hd'] = list()
+    metric_dict['nsd'] = list()
+    
+    for image_path in tqdm(image_list):
+        case_name = image_path.split('/')[-2]
+        id = image_path.split('/')[-1]
+        
+        h5f = h5py.File(image_path, 'r')
+        image = h5f['image'][:]
+        label = h5f['label'][:]
+
+        with torch.no_grad():
+            prediction, score_map = test_single_case_patch(net, image, 18,18, 4, patch_shape , num_classes=num_classes)
+            
+        if np.sum(prediction) == 0:
+            single_metric = (0,0,0,0,0,0)
+        else:
+            if has_post:
+                print('post')
+                prediction = getLargestCC(prediction)
+            single_metric = calculate_metric_percase(prediction, label[:], space=FLAGS.spacing)
+            metric_dict['name'].append(case_name)
+            metric_dict['dice'].append(single_metric[0])
+            metric_dict['jaccard'].append(single_metric[1])
+            metric_dict['asd'].append(single_metric[2])
+            metric_dict['95hd'].append(single_metric[3])
+            metric_dict['hd'].append(single_metric[4])
+            metric_dict['nsd'].append(single_metric[5])
+            print(case_name,single_metric)
+
+
+        # total_metric += np.asarray(single_metric)
+
+        if save_result:
+            test_save_path_temp = os.path.join(test_save_path, case_name)
+            if not os.path.exists(test_save_path_temp):
+                os.makedirs(test_save_path_temp)
+            
+            nib.save(nib.Nifti1Image(prediction.astype(np.uint8), np.eye(4)), test_save_path_temp + '/' + id + "_pred.nii.gz")
+            nib.save(nib.Nifti1Image(image.astype(np.float32), np.eye(4)), test_save_path_temp + '/' +  id + "_img.nii.gz")
+            nib.save(nib.Nifti1Image(label.astype(np.uint8), np.eye(4)), test_save_path_temp + '/' + id + "_gt.nii.gz")
+    
+    avg_metric = total_metric / len(image_list)
+    metric_csv = pd.DataFrame(metric_dict)
+    if has_post:
+        metric_csv.to_csv(test_save_path + '/metric_post_' + str(FLAGS.epoch_num) + '.csv', index=False)
+    else:
+        metric_csv.to_csv(test_save_path + '/metric_'+str(FLAGS.epoch_num)+'.csv', index=False)
+    print('average metric is {}'.format(avg_metric))
+
+    return avg_metric
+
+def test_single_case_patch(net, image, stride_x,stride_y, stride_z, patch_size, num_classes=1):
+    w, h, d = image.shape
+
+    # if the size of image is less than patch_size, then padding it
+    add_pad = False
+    if w < patch_size[0]:
+        w_pad = patch_size[0]-w
+        add_pad = True
+    else:
+        w_pad = 0
+    if h < patch_size[1]:
+        h_pad = patch_size[1]-h
+        add_pad = True
+    else:
+        h_pad = 0
+    if d < patch_size[2]:
+        d_pad = patch_size[2]-d
+        add_pad = True
+    else:
+        d_pad = 0
+    wl_pad, wr_pad = w_pad//2,w_pad-w_pad//2
+    hl_pad, hr_pad = h_pad//2,h_pad-h_pad//2
+    dl_pad, dr_pad = d_pad//2,d_pad-d_pad//2
+    if add_pad:
+        image = np.pad(image, [(wl_pad,wr_pad),(hl_pad,hr_pad), (dl_pad, dr_pad)], mode='constant', constant_values=0)
+    ww,hh,dd = image.shape
+
+    sx = math.ceil((ww - patch_size[0]) / stride_x) + 1
+    sy = math.ceil((hh - patch_size[1]) / stride_y) + 1
+    sz = math.ceil((dd - patch_size[2]) / stride_z) + 1
+    # print("{}, {}, {}".format(sx, sy, sz))
+    score_map = np.zeros((num_classes, ) + image.shape).astype(np.float32)
+    cnt = np.zeros(image.shape).astype(np.float32)
+    
+
+    for x in range(0, sx):
+        xs = min(stride_x*x, ww-patch_size[0])
+        for y in range(0, sy):
+            ys = min(stride_y * y,hh-patch_size[1])
+            for z in range(0, sz):
+                zs = min(stride_z * z, dd-patch_size[2])
+                test_patch = image[xs:xs+patch_size[0], ys:ys+patch_size[1], zs:zs+patch_size[2]]
+                test_patch = np.expand_dims(np.expand_dims(test_patch,axis=0),axis=0).astype(np.float32)
+                test_patch = torch.from_numpy(test_patch).cuda()
+
+                y_1, y_2 = net(test_patch)
+                
+                y_1_soft = F.softmax(y_1, dim=1)
+                y_2_soft = F.softmax(y_2, dim=1)
+                y = torch.mean(torch.stack([y_1_soft, y_2_soft]), dim=0)
+                y = y.cpu().data.numpy()
+                y = y[0,:,:,:,:]
+                
+                score_map[:, xs:xs+patch_size[0], ys:ys+patch_size[1], zs:zs+patch_size[2]] \
+                  = score_map[:, xs:xs+patch_size[0], ys:ys+patch_size[1], zs:zs+patch_size[2]] + y
+                cnt[xs:xs + patch_size[0], ys:ys + patch_size[1], zs:zs + patch_size[2]] \
+                    = cnt[xs:xs + patch_size[0], ys:ys + patch_size[1], zs:zs + patch_size[2]] + 1
+                
+    score_map = score_map/np.expand_dims(cnt,axis=0)
+    label_map = np.argmax(score_map, axis = 0)
+    if add_pad:
+        label_map = label_map[wl_pad:wl_pad+w,hl_pad:hl_pad+h,dl_pad:dl_pad+d]
+        score_map = score_map[:,wl_pad:wl_pad+w,hl_pad:hl_pad+h,dl_pad:dl_pad+d]
+        
+    return label_map, score_map
+
+def cal_dice(prediction, label, num=2):
+    total_dice = np.zeros(num-1)
+    for i in range(1, num):
+        prediction_tmp = (prediction==i)
+        label_tmp = (label==i)
+        prediction_tmp = prediction_tmp.astype(np.float)
+        label_tmp = label_tmp.astype(np.float)
+
+        dice = 2 * np.sum(prediction_tmp * label_tmp) / (np.sum(prediction_tmp) + np.sum(label_tmp))
+        total_dice[i - 1] += dice
+
+    return total_dice
+
+
+def calculate_metric_percase(pred, gt, space=0.625):
+    dice = dc(pred, gt)
+    jc_score = jc(pred, gt)
+    dice_medpy = metric.binary.dc(pred,gt)
+    jc_medpy = metric.binary.jc(pred,gt)
+    print(dice_medpy,jc_medpy)
+    asd_score = asd(pred, gt, voxelspacing=space)
+    hd95_score = hd95(pred, gt, voxelspacing=space)
+    hd_score = hd(pred, gt, voxelspacing=space)
+    nsd = normalized_surface_dice(pred, gt, voxelspacing=space)
+    return dice, jc_score, asd_score, hd95_score, hd_score, nsd
+
+
+def normalized_surface_dice(pred, gt, voxelspacing=None):
+    surface_dis = surf_metric.compute_surface_distances(gt.astype(np.bool),
+                                                        pred.astype(np.bool),
+                                                        spacing_mm=(voxelspacing, voxelspacing, voxelspacing))
+    surface_dice = surf_metric.compute_surface_dice_at_tolerance(surface_dis, 1)
+    return surface_dice
+
+
+def getLargestCC(segmentation):
+    labels = label(segmentation)
+    assert(labels.max() != 0) # assume at least 1 CC
+    largestCC = labels == np.argmax(np.bincount(labels.flat)[1:])+1
+    largestCC=np.array(largestCC,dtype=np.int)
+    
+    return largestCC
+
+
+if __name__ == '__main__':
+    metric = test_calculate_metric(FLAGS.epoch_num)
+    
---
+++ code/train_LA_semi_contrastive.py
@@ -0,0 +1,225 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from tqdm import tqdm
+from tensorboardX import SummaryWriter
+import shutil
+import argparse
+import logging
+import time
+import random
+import numpy as np
+
+import torch
+import torch.optim as optim
+from torchvision import transforms
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+from torch.nn import CosineSimilarity
+from torch.utils.data import DataLoader
+
+from networks.vnet_center import VNet, center_model
+
+from utils import losses
+from dataloaders.la_heart import LAHeart, RandomCrop, RandomRotFlip, ToTensor, TwoStreamBatchSampler
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--root_path', type=str, default='../data/2018LA_Seg_Training Set/', help='data path')
+parser.add_argument('--model', type=str,  default='SCC', help='model_name')
+parser.add_argument('--max_iterations', type=int,  default=6000, help='maximum iteration number to train')
+parser.add_argument('--batch_size', type=int, default=4, help='batch_size per gpu')
+parser.add_argument('--labeled_bs', type=int, default=2, help='labeled_batch_size per gpu')
+parser.add_argument('--base_lr', type=float,  default=0.01, help='maximum epoch number to train')
+parser.add_argument('--deterministic', type=int,  default=1, help='whether use deterministic training')
+parser.add_argument('--labelnum', type=int,  default=16, help='number of labeled data')
+parser.add_argument('--seed', type=int,  default=2022, help='random seed')
+parser.add_argument('--gpu', type=str,  default='3', help='GPU to use')
+
+
+# loss
+parser.add_argument('--my_lambda', type=float,  default=1, help='balance factor to control contrastive loss')
+parser.add_argument('--tau', type=float,  default=1, help='temperature of the contrastive loss')
+
+parser.add_argument('--has_contrastive', type=int,  default=1, help='whether use contrative loss')
+parser.add_argument('--only_supervised', type=int,  default=0, help='whether use consist loss')
+
+args = parser.parse_args()
+exp_time = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time()))
+
+train_data_path = args.root_path
+snapshot_path = "../semi_model/" + args.model + "/" + exp_time + "/"
+
+
+os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+batch_size = args.batch_size * len(args.gpu.split(','))
+max_iterations = args.max_iterations
+base_lr = args.base_lr
+labeled_bs = args.labeled_bs
+
+if not args.deterministic:
+    cudnn.benchmark = True #
+    cudnn.deterministic = False #
+else:
+    cudnn.benchmark = False  # True #
+    cudnn.deterministic = True  # False #
+random.seed(args.seed)
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed(args.seed)
+
+num_classes = 2
+patch_size = (112, 112, 80)
+
+
+if __name__ == "__main__":
+    ## make logger file
+    if not os.path.exists(snapshot_path):
+        os.makedirs(snapshot_path)
+    if os.path.exists(snapshot_path + '/code'):
+        shutil.rmtree(snapshot_path + '/code')
+    shutil.copytree('.', snapshot_path + '/code', shutil.ignore_patterns(['.git','__pycache__']))
+
+    logging.basicConfig(filename=snapshot_path+"/log.txt", level=logging.INFO,
+                        format='[%(asctime)s.%(msecs)03d] %(message)s', datefmt='%H:%M:%S')
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+    logging.info(str(args))
+
+    # Network definition
+    # E2DNet for segmentation
+    model = VNet(n_channels=1, n_classes=num_classes, n_filters=16, normalization='batchnorm', has_dropout=True, has_ddecoder=True)
+    model.cuda()
+    # classification model
+    center_model = center_model(num_classes=num_classes,ndf=64)
+    center_model.cuda()
+    db_train = LAHeart(base_dir=train_data_path,
+                       split='train80', # train/val split
+                       # num=args.labelnum,#16,
+                       transform = transforms.Compose([
+                          RandomRotFlip(),
+                          RandomCrop(patch_size),
+                          ToTensor(),
+                          ]))
+
+    labelnum = args.labelnum    # default 16
+    label_idx = list(range(0,80))
+    random.shuffle(label_idx)
+    labeled_idxs = label_idx[:labelnum]
+    unlabeled_idxs = label_idx[labelnum:80]
+    batch_sampler = TwoStreamBatchSampler(labeled_idxs, unlabeled_idxs, batch_size, batch_size-labeled_bs)
+    def worker_init_fn(worker_id):
+        random.seed(args.seed+worker_id)
+    trainloader = DataLoader(db_train, batch_sampler=batch_sampler, num_workers=4, pin_memory=True,worker_init_fn=worker_init_fn)
+
+    model.train()
+
+    optimizer = optim.SGD(model.parameters(), lr=base_lr, momentum=0.9, weight_decay=0.0001)
+    cos_sim = CosineSimilarity(dim=1,eps=1e-6)
+
+    writer = SummaryWriter(snapshot_path+'/log')
+    logging.info("{} itertations per epoch".format(len(trainloader)))
+
+    iter_num = 0
+    max_epoch = max_iterations//len(trainloader)+1
+    lr_ = base_lr
+
+    iterator = tqdm(range(max_epoch), ncols=70)
+    for epoch_num in iterator:
+        time1 = time.time()
+        for i_batch, sampled_batch in enumerate(trainloader):
+            time2 = time.time()
+            # print('fetch data cost {}'.format(time2-time1))
+            volume_batch, label_batch = sampled_batch['image'], sampled_batch['label']
+            volume_batch, label_batch = volume_batch.cuda(), label_batch.cuda()
+
+            outputs_1, outputs_2 = model(volume_batch)
+            outputs_soft_1 = F.softmax(outputs_1,dim=1)
+            outputs_soft_2 = F.softmax(outputs_2,dim=1)
+            
+            '''
+            # fully supervised manner
+            loss_seg_1 = F.cross_entropy(outputs_1, label_batch)#ce_loss(outputs_1[:labeled_bs, 0, ...], label_batch[:labeled_bs].float())
+            loss_seg_dice_1 = losses.dice_loss(outputs_soft_1[:, 1, :, :, :], label_batch == 1)
+            loss_seg_2 = F.cross_entropy(outputs_2, label_batch)
+            loss_seg_dice_2 = losses.dice_loss(outputs_soft_2[:,1, :, :, :], label_batch == 1)
+            '''
+            ## calculate the supervised loss
+            loss_seg_1 = F.cross_entropy(outputs_1[:labeled_bs, ...], label_batch[:labeled_bs])#ce_loss(outputs_1[:labeled_bs, 0, ...], label_batch[:labeled_bs].float())
+            loss_seg_dice_1 = losses.dice_loss(outputs_soft_1[:labeled_bs, 1, :, :, :], label_batch[:labeled_bs] == 1)
+            loss_seg_2 = F.cross_entropy(outputs_2[:labeled_bs, ...], label_batch[:labeled_bs])
+            loss_seg_dice_2 = losses.dice_loss(outputs_soft_2[:labeled_bs,1, :, :, :], label_batch[:labeled_bs] == 1)
+            
+            supervised_loss = 0.5*(loss_seg_dice_1 + loss_seg_dice_2) + 0.5*(loss_seg_1 + loss_seg_2)
+            
+            if args.has_contrastive == 1:
+                
+                create_center_1_bg = center_model(outputs_1[:,0,...].unsqueeze(1))# 4,1,x,y,z->4,2
+                create_center_1_la = center_model(outputs_1[:,1,...].unsqueeze(1))
+                create_center_2_bg = center_model(outputs_2[:,0,...].unsqueeze(1))
+                create_center_2_la = center_model(outputs_2[:,1,...].unsqueeze(1))
+        
+                create_center_soft_1_bg = F.softmax(create_center_1_bg, dim=1)# dims(4,2)
+                create_center_soft_1_la = F.softmax(create_center_1_la, dim=1)
+                create_center_soft_2_bg = F.softmax(create_center_2_bg, dim=1)# dims(4,2)
+                create_center_soft_2_la = F.softmax(create_center_2_la, dim=1)
+                
+                lb_center_12_bg = torch.cat((create_center_soft_1_bg[:labeled_bs,...], create_center_soft_2_bg[:labeled_bs,...]),dim=0)# 4,2
+                lb_center_12_la = torch.cat((create_center_soft_1_la[:labeled_bs,...], create_center_soft_2_la[:labeled_bs,...]),dim=0)
+                un_center_12_bg = torch.cat((create_center_soft_1_bg[labeled_bs:,...], create_center_soft_2_bg[labeled_bs:,...]),dim=0)
+                un_center_12_la = torch.cat((create_center_soft_1_la[labeled_bs:,...], create_center_soft_2_la[labeled_bs:,...]),dim=0)
+               
+
+                # cosine similarity
+                loss_contrast = losses.scc_loss(cos_sim, args.tau, lb_center_12_bg,lb_center_12_la, un_center_12_bg, un_center_12_la)
+                loss = supervised_loss + args.my_lambda * loss_contrast
+                
+            if args.only_supervised==1:
+                print('only supervised')
+                loss = supervised_loss
+            
+           
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+
+            iter_num = iter_num + 1
+            writer.add_scalar('lr', lr_, iter_num)
+            writer.add_scalar('loss/loss', loss, iter_num)
+            writer.add_scalar('loss/loss_seg', loss_seg_1+loss_seg_2, iter_num)
+            writer.add_scalar('loss/loss_dice', loss_seg_dice_1+loss_seg_dice_2, iter_num)
+            writer.add_scalar('loss/loss_supervised', supervised_loss, iter_num)
+            if args.has_contrastive == 1:
+                writer.add_scalar('loss/loss_contrastive', loss_contrast, iter_num)
+            
+
+
+            logging.info(
+                'iteration %d : loss : %f, loss_seg_1: %f, loss_seg_2: %f, loss_dice_1: %f, loss_dice_2: %f' %
+                (iter_num, loss.item(),   loss_seg_1.item(), loss_seg_2.item(), loss_seg_dice_1.item(), loss_seg_dice_2.item()))
+            if args.has_contrastive == 1:
+                logging.info(
+                'iteration %d : supervised loss : %f, contrastive loss: %f' %
+                (iter_num, supervised_loss.item(),  loss_contrast.item()))
+
+            ## change lr
+            # lr_ = base_lr * (1.0 - iter_num / max_iterations) ** 0.9
+            # for param_group in optimizer.param_groups:
+            #     param_group['lr'] = lr_
+            if iter_num % 2500 == 0:
+                lr_ = base_lr * 0.1 ** (iter_num // 2500)
+                for param_group in optimizer.param_groups:
+                    param_group['lr'] = lr_
+            ## save checkpoint
+            if iter_num % 1000 == 0:
+                save_mode_path = os.path.join(snapshot_path, 'iter_' + str(iter_num) + '.pth')
+                torch.save(model.state_dict(), save_mode_path)
+                logging.info("save model to {}".format(save_mode_path))
+
+            if iter_num >= max_iterations:
+                break
+            time1 = time.time()
+        if iter_num >= max_iterations:
+            iterator.close()
+            break
+    writer.close()
